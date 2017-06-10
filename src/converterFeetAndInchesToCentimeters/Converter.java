405cf29d61e0b0688f7bb60e1b03161e08fc6995
Ross <Ross@MacBook-Pro-R.home>
2017-06-10 17:03:43 -0400

    Initial commit

---
+++ src/converterFeetAndInchesToCentimeters/Converter.java
@@ -0,0 +1,34 @@
+package converterFeetAndInchesToCentimeters;
+
+public class Converter {
+
+    public static void main(String[] args) {
+    	double centimeters = converterFeetAndInchesToCentimeters(4,1);
+    	if(centimeters <0.0) {
+            System.out.println("Invalid parameters");
+        }
+        converterFeetAndInchesToCentimeters(55);
+    }
+    
+    public static double converterFeetAndInchesToCentimeters(double feet, double inches) {
+    	if((feet <0) || ((inches <0) || (inches >12))) {
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
+    }
+
+    public static double converterFeetAndInchesToCentimeters(double inches) {
+    	if(inches < 0) {
+    		return -1;
+    	}
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + " inches");
+        return converterFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+}
